ea018028f1f10f4bdf11a6c2d08c2b0662f063f7
awandev <awal.kurniawan@uin-alauddin.ac.id>
2020-06-16 22:51:56 +0800

    add helper function for navigation

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require("http");
 const express = require("express");
 const bodyParser = require("body-parser");
-
+const path = require('path')
 const app = express();
 
 const adminRoutes = require('./routes/admin');
@@ -14,7 +14,7 @@ app.use('/shop',shopRoutes)
 app.use('/admin', adminRoutes)
 
 app.use((req, res, next) => {
-  res.status(404).send('<h1>Page Not Found</h1>')
+  res.status(404).sendFile(path.join(__dirname, 'views','404.html'))
 })
 
 app.listen(3000)
