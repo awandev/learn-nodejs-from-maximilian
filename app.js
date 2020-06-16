66bcdd82730df89b32035dc93a64c5d6f226ab18
awandev <awal.kurniawan@uin-alauddin.ac.id>
2020-06-16 13:43:24 +0800

    add path to routes

--- app.js
+++ app.js
@@ -5,13 +5,13 @@ const bodyParser = require("body-parser");
 const app = express();
 
 const adminRoutes = require('./routes/admin');
-const showRoutes = require('./routes/shop')
+const shopRoutes = require('./routes/shop')
 
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(showRoutes)
-app.use(adminRoutes)
+app.use('/shop',shopRoutes)
+app.use('/admin', adminRoutes)
 
 app.use((req, res, next) => {
   res.status(404).send('<h1>Page Not Found</h1>')
