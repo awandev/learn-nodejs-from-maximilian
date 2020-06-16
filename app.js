b80bbb9556f842c98923da11b2ed2669ed882336
awandev <awal.kurniawan@uin-alauddin.ac.id>
2020-06-16 13:28:35 +0800

    first push

---
+++ app.js
@@ -0,0 +1,20 @@
+const http = require("http");
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+const adminRoutes = require('./routes/admin');
+const showRoutes = require('./routes/shop')
+
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(showRoutes)
+app.use(adminRoutes)
+
+app.use((req, res, next) => {
+  res.status(404).send('<h1>Page Not Found</h1>')
+})
+
+app.listen(3000)
