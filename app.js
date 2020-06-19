71803478ac0c185c9d74009af7c519e72505fc43
awandev <awal.kurniawan@uin-alauddin.ac.id>
2020-06-19 15:09:49 +0800

    add pug template engine

--- app.js
+++ app.js
@@ -1,21 +1,24 @@
-const http = require("http");
-const express = require("express");
-const bodyParser = require("body-parser");
-const path = require('path')
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop')
+app.set('view engine', 'pug');
+app.set('views', 'views');
 
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname,'public')))
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/shop',shopRoutes)
-app.use('/admin', adminRoutes)
+app.use('/admin', adminData.routes);
+app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, 'views','404.html'))
-})
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+});
 
-app.listen(3000)
+app.listen(3000);
